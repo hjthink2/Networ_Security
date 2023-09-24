682b8c70a0011d7e760df8187e3cec0cf391f4c3
hjthink2 <79956361+hjthink2@users.noreply.github.com>
2023-09-24 20:07:30 +0900

    Add files via upload

---
+++ project.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+
+
+/* Ethernet header */
+struct ethheader {
+  u_char  ether_dhost[6]; /* destination host address */
+  u_char  ether_shost[6]; /* source host address */
+  u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+};
+
+/* IP Header */
+struct ipheader {
+  unsigned char      iph_ihl:4, //IP header length
+                     iph_ver:4; //IP version
+  unsigned char      iph_tos; //Type of service
+  unsigned short int iph_len; //IP Packet length (data + header)
+  unsigned short int iph_ident; //Identification
+  unsigned short int iph_flag:3, //Fragmentation flags
+                     iph_offset:13; //Flags offset
+  unsigned char      iph_ttl; //Time to Live
+  unsigned char      iph_protocol; //Protocol type
+  unsigned short int iph_chksum; //IP datagram checksum
+  struct  in_addr    iph_sourceip; //Source IP address
+  struct  in_addr    iph_destip;   //Destination IP address
+};
+
+/* TCP Header */
+struct tcpheader {
+  unsigned short int tcph_srcport; // Source port
+  unsigned short int tcph_destport; // Destination port
+};
+
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+                              const u_char *packet)
+{
+  struct ethheader *eth = (struct ethheader *)packet;
+
+  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
+    struct ipheader * ip = (struct ipheader *)
+                           (packet + sizeof(struct ethheader)); 
+    if (ip->iph_protocol == IPPROTO_TCP) {
+      struct tcpheader *tcp = (struct tcpheader *)
+                              (packet + sizeof(struct ethheader) + sizeof(struct ipheader));
+      printf("\n======Ethernet Header======\n");
+      printf("Src  MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+             eth->ether_shost[0], eth->ether_shost[1], eth->ether_shost[2],
+             eth->ether_shost[3], eth->ether_shost[4], eth->ether_shost[5]);
+      printf("Dst  MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+             eth->ether_dhost[0], eth->ether_dhost[1], eth->ether_dhost[2],
+             eth->ether_dhost[3], eth->ether_dhost[4], eth->ether_dhost[5]);
+           
+      printf("\n======IP Header======\n");
+      printf("Src   IP: %s\n", inet_ntoa(ip->iph_sourceip));   
+      printf("Dst   IP: %s\n", inet_ntoa(ip->iph_destip));    
+                              
+      printf("\n======TCP Header======\n");
+      printf("Src Port: %d\n", ntohs(tcp->tcph_srcport));
+      printf("Dsc Port: %d\n\n\n", ntohs(tcp->tcph_destport));
+    }
+  }
+}
+
+
+int main()
+{
+  pcap_t *handle;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  struct bpf_program fp;
+  char filter_exp[] = "";
+  bpf_u_int32 net;
+
+  // Step 1: Open live pcap session on NIC with name enp0s3
+  handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
+
+  // Step 2: Compile filter_exp into BPF psuedo-code
+  pcap_compile(handle, &fp, filter_exp, 0, net);
+  if (pcap_setfilter(handle, &fp) !=0) {
+      pcap_perror(handle, "Error:");
+      exit(EXIT_FAILURE);
+  }
+
+  // Step 3: Capture packets
+  pcap_loop(handle, -1, got_packet, NULL);
+
+  pcap_close(handle);   //Close the handle
+  return 0;
+}
